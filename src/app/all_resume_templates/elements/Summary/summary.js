566635336233c85150054fb961c8bb5fd976879a
avisivia <avisivia@gmail.com>
2023-09-17 18:23:22 -0500

    main

--- src/app/all_resume_templates/elements/Summary/summary.js
+++ src/app/all_resume_templates/elements/Summary/summary.js
@@ -12,8 +12,7 @@ export default function Summary() {
                     <IoIosContact size={35} className={Style.icon} />
                     ABOUT ME</div>
             </div>
-            <div className={Style.summary}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.
+            <div className={Style.summary}>Lorem Ipsum.
             </div>
         </div>
     )
